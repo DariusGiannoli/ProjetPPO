1c10dfc5c1a1478b018a29055cc24190b95a744e
alepin <antoine.lepin@epfl.ch>
2025-02-18 09:55:01 +0100

    Stop

---
+++ ReCHor/src/ch/epfl/rechor/journey/Journey.java
@@ -0,0 +1,4 @@
+package ch.epfl.rechor.journey;
+
+public record Journey() {
+}
---
+++ ReCHor/src/ch/epfl/rechor/journey/Stop.java
@@ -0,0 +1,13 @@
+package ch.epfl.rechor.journey;
+
+import ch.epfl.rechor.Preconditions;
+
+import java.util.Objects;
+
+public record Stop(String name, String platformName, double longitude, double latitude) {
+
+    public Stop {
+        Objects.requireNonNull(name);
+        Preconditions.checkArgument(longitude < -180 || longitude > 180 || latitude > 90 || latitude < -90);
+    }
+}
