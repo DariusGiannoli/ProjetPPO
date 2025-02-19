b0dd8e488d85446f4ecdace4707a9129d5f1894e
alepin <antoine.lepin@epfl.ch>
2025-02-19 18:21:22 +0100

    Token Antoine

--- ReCHor/src/ch/epfl/rechor/FormatterFr.java
+++ ReCHor/src/ch/epfl/rechor/FormatterFr.java
@@ -46,20 +46,20 @@ public final class FormatterFr {
     public static String formatTime(LocalDateTime dateTime) {
         // Ex. on veut "jour/mois/année heurehminute"
         // On peut construire un DateTimeFormatter personnalisé :
-        /*DateTimeFormatter dateFormatter = new DateTimeFormatterBuilder()
+        /*
+        DateTimeFormatter dateFormatter = new DateTimeFormatterBuilder()
                 .appendValue(ChronoField.DAY_OF_MONTH)
                 .appendLiteral('/')
                 .appendValue(ChronoField.MONTH_OF_YEAR)
                 .appendLiteral('/')
                 .appendValue(ChronoField.YEAR)
                 .toFormatter();
-
          */
 
         String hourPart = dateTime.getHour() + "h" + String.format("%02d", dateTime.getMinute());
 
-        // On assemble date + heure
-        return /*dateFormatter.format(dateTime) + " " +*/ hourPart;
+
+        return /* dateFormatter.format(dateTime) + " " + */ hourPart;
     }
 
     /**
