09cc04ecd77cebe044f1d59c1b9caf5b38b29e8b
alepin <antoine.lepin@epfl.ch>
2025-02-20 20:40:46 +0100

    3.5(encore a tester et à faire dominatesOrIsEqual)

---
+++ ReCHor/src/ch/epfl/rechor/journey/PackedCriteria.java
@@ -0,0 +1,76 @@
+package ch.epfl.rechor.journey;
+
+import ch.epfl.rechor.Preconditions;
+
+public class PackedCriteria {
+
+    private PackedCriteria() {}
+
+    public static long pack(int arrMins, int changes, int payload){
+        Preconditions.checkArgument((changes >> 7) == 0 && (arrMins >> 12) == 0);
+        long pack = (((long) arrMins) << 39) + (((long) changes) << 32) + payload;
+        return pack;
+    }
+
+    public static boolean hasDepMins(long criteria){
+        long hasDep = criteria >>> 51;
+        return hasDep >= (4095 - 3119) && hasDep <= 4095;
+    }
+
+    public static int depMins(long criteria){
+        long dep = criteria >>> 51;
+        Preconditions.checkArgument(dep >= (4095 - 3119) && dep <= 4095);
+
+        return (int) (-dep + 4095 - 240);
+    }
+
+    public static int arrMins(long criteria){
+        long arr = (criteria >>> 39) & 4095;
+
+        return (int) (arr - 240);
+    }
+
+    public static int changes(long criteria){
+        long change = (criteria >>> 32) & 127;
+
+        return (int) change;
+    }
+
+    public static int payload(long criteria){
+        long payload = criteria & 0xffffffffL;
+        return (int) payload;
+    }
+
+    public static boolean dominatesOrIsEqual(long criteria1, long criteria2){
+        //à faire
+        return true;
+    }
+
+    public static long withoutDepMins(long criteria){
+        long newCriteria = criteria & (~((0b111111111111L) << 51));
+        return newCriteria;
+    }
+
+    public static long withDepMins(long criteria, int depMins1){
+        long newCriteria = withoutDepMins(criteria);
+        newCriteria = newCriteria | (((long) depMins1) << 51);
+        return newCriteria;
+    }
+
+    public static long withAdditionalChange(long criteria){
+        int changes = changes(criteria) + 1;
+        long newCriteria = criteria & (~((0b1111111L) << 32));
+        newCriteria = newCriteria | (((long) changes) << 32);
+
+        return newCriteria;
+    }
+
+    public static long withPayload(long criteria, int payload1){
+        long newCriteria = criteria & (~0xffffffffL);
+        newCriteria = newCriteria | ((long) payload1);
+
+        return newCriteria;
+    }
+
+
+}
