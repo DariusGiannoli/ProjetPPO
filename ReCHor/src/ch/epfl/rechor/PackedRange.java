e702e4a11ec7e664e477797ea8b3914b38974914
DariusGiannoli <152925066+DariusGiannoli@users.noreply.github.com>
2025-03-31 18:06:21 +0200

    PackedRange ok

--- ReCHor/src/ch/epfl/rechor/PackedRange.java
+++ ReCHor/src/ch/epfl/rechor/PackedRange.java
@@ -3,7 +3,9 @@ package ch.epfl.rechor;
 /**
  * Classe utilitaire (non instanciable) permettant de manipuler des intervalles
  * d'entiers empaquetés dans un int. La borne inférieure occupe 24 bits et la longueur
- * de l'intervalle occupe 8 bits
+ * de l'intervalle occupe 8 bits.
+ * Cette représentation compacte est utilisée pour optimiser la recherche de voyages.
+ *
  * @author Antoine Lepin (390950)
  * @author Darius Giannoli (380759)
  */
@@ -14,9 +16,10 @@ public final class PackedRange {
     private PackedRange() {}
 
     /**
-     * Empaquète l'intervalle d'entiers dans in int
+     * Empaquète l'intervalle d'entiers dans un int.
+     *
      * @param startInclusive borne inférieure (incluse)
-     * @param endExclusive  borne supérieure (exclue)
+     * @param endExclusive   borne supérieure (exclue)
      * @return l'entier 32 bits représentant l'intervalle empaqueté
      */
     public static int pack(int startInclusive, int endExclusive){
@@ -25,8 +28,9 @@ public final class PackedRange {
     }
 
     /**
-     * Retourne la longueur de l'intervalle empaqueté
-     * @param interval  l'intervalle empaqueté
+     * Retourne la longueur de l'intervalle empaqueté.
+     *
+     * @param interval l'intervalle empaqueté
      * @return la longueur de l'intervalle
      */
     public static int length(int interval){
@@ -34,20 +38,22 @@ public final class PackedRange {
     }
 
     /**
-     * Retourne la borne inférieure (incluse) de l'intervalle empaqueté
+     * Retourne la borne inférieure (incluse) de l'intervalle empaqueté.
+     *
      * @param interval l'intervalle empaqueté
-     * @return  la borne inférieure, incluse
+     * @return la borne inférieure, incluse
      */
     public static int startInclusive(int interval){
         return Bits32_24_8.unpack24(interval);
     }
 
     /**
-     * Retourne la borne supérieure (exclue) de l'intervalle empaqueté
-     * @param interval  l'intervalle empaqueté
-     * @return  la borne supérieure, exclue
+     * Retourne la borne supérieure (exclue) de l'intervalle empaqueté.
+     *
+     * @param interval l'intervalle empaqueté
+     * @return la borne supérieure, exclue
      */
     public static int endExclusive(int interval){
-        return startInclusive(interval)+ length(interval);
+        return startInclusive(interval) + length(interval);
     }
 }
